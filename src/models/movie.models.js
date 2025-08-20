ec1a7534587d2360c75435707206545402435892
Satya Yalla <yallasatyamani@gmail.com>
2025-08-21 01:38:47 +0530

    added showtime model and delete, virtual in movieSchema

--- src/models/movie.models.js
+++ src/models/movie.models.js
@@ -14,10 +14,33 @@ const movieSchema = new mongoose.Schema({
         required: true
     },
     posterImage: {
-        type: String,
+        type: String, // URL is string, we upload using cloudinary and multer
         required: true
     },
 }, {timestamps: true})
+
+
+// Lets you populate all showtimes for a movie.
+// i.e., for a movie, it will pull-out all showtimes
+
+movieSchema.virtual("showtimes", {
+    ref: "Showtime",           // reference model
+    localField: "_id",         // movie._id
+    foreignField: "movieId"    // showtime.movieId
+});
+
+// if a movie is deleted, deletes its showtimes too
+// we use middleware
+
+movieSchema.pre("findOneAndDelete", async function (next){
+    try {
+        const movieId = this.getQuery()["_id"];
+        await mongoose.model("Showtime").deleteMany({movieId});
+        next();
+    } catch (err) {
+        next(err);
+    }
+})
 
 
 export const Movie = mongoose.model("Movie", movieSchema)
---
+++ src/models/showtime.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const showtimeSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    capacity: {
+        type: Number, // available seats
+        required: true
+    },
+
+    // each showtime is linked to a movie through movieId
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+        required: true
+    }
+}, { timestamps: true})
+
+
+export const Showtime = mongoose.model("Showtime", showtimeSchema);
